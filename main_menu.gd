440fbcbc8ddfd77fb118e2d7163071994d9d6598
AKGamerTech <bodys0331@gmail.com>
2026-06-21 19:51:27 +0200

    main menu added

---
+++ main_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+@export var start_button: Button
+
+@export var settings_button: Button
+@export var background_settings: ColorRect
+@export var exit_settings: Button
+
+@export var about_button: Button
+@export var exit_button: Button
+
+func _ready():
+	start_button.pressed.connect(start)
+	settings_button.pressed.connect(settings)
+	exit_settings.pressed.connect(settings_exit)
+	about_button.pressed.connect(about)
+	exit_button.pressed.connect(exit)
+	
+func start() :
+	get_tree().change_scene_to_file("res://background_generator.tscn")
+
+func settings():
+	background_settings.visible = !background_settings.visible
+	
+func settings_exit():
+	background_settings.visible = !background_settings.visible
+
+func about():
+	pass
+	
+func exit():
+	get_tree().quit()
